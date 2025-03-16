13cc8ca6cfd4f940141e0a3b729e779b9ad06bf0
swayam patial <swayampatial1@gmail.com>
2025-03-17 00:42:43 +0530

    first commit

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,6 +1,17 @@
+import Link from 'next/link';
+import { Button } from '../ui/button';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { LuArmchair } from 'react-icons/lu';
+import { VscCode } from 'react-icons/vsc';
+
 function Logo() {
   return (
-    <div>Logo</div>
-  )
+    <Button size='icon' asChild>
+      <Link href='/'>
+        <VscCode className='w-6 h-6' />
+      </Link>
+    </Button>
+  );
 }
-export default Logo
+
+export default Logo;
