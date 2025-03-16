5e5df0a3bb3914273b320285a862181421116d5b
swayam patial <swayampatial1@gmail.com>
2025-03-17 01:25:21 +0530

    first commit

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,14 +1,12 @@
 import Link from 'next/link';
 import { Button } from '../ui/button';
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { LuArmchair } from 'react-icons/lu';
-import { VscCode } from 'react-icons/vsc';
+import { DiDebian } from "react-icons/di";
 
 function Logo() {
   return (
     <Button size='icon' asChild>
       <Link href='/'>
-        <VscCode className='w-6 h-6' />
+        <DiDebian className='w-6 h-6' />
       </Link>
     </Button>
   );
