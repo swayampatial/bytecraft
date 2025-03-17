47e93eaea4b35432784f6c73b4fddd25de444f7a
swayam patial <swayampatial1@gmail.com>
2025-03-17 23:22:53 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Container from "@/components/global/Container";
+import Providers from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Providers>
          <Navbar />
          <Container className='py-20'>{children}</Container>
+         </Providers>
       </body>
     </html>
   );
