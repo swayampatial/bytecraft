591ba31c9959f3a71e0c46cfbb3e968bb4a1ec04
swayam patial <swayampatial1@gmail.com>
2025-03-18 01:24:12 +0530

    first commit

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,16 +1,18 @@
 import Logo from './Logo';
 import LinksDropdown from './LinksDropdown';
+import DarkMode from './DarkMode';
 import CartButton from './CartButton';
-import Container from '../global/Container';
 import NavSearch from './NavSearch';
+import Container from '../global/Container';
 function Navbar() {
   return (
     <nav className='border-b '>
       <Container className='flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-4 py-8'>
         <Logo />
-        <NavSearch/>
+        <NavSearch />
         <div className='flex gap-4 items-center '>
           <CartButton />
+          <DarkMode />
           <LinksDropdown />
         </div>
       </Container>
