2145f3cff6d1423f25469933d3b8d9bf531b60ce
swayam patial <swayampatial1@gmail.com>
2025-03-30 19:07:31 +0530

    first commit

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,17 @@
-function ProductsPage() {
-  return (
-    <div>ProductsPage</div>
-  )
+import ProductsContainer from "@/components/products/ProductsContainer";
+
+interface ProductsPageProps {
+  searchParams: {
+    layout?: string;
+    search?: string;
+  };
 }
-export default ProductsPage
+
+const ProductsPage = ({ searchParams }: ProductsPageProps) => {
+  const layout = searchParams?.layout || "grid";
+  const search = searchParams?.search || "";
+
+  return <ProductsContainer layout={layout} search={search} />;
+};
+
+export default ProductsPage;
