8dcbbdc4580e562f093ff67f4cc7bb09720dc259
swayam patial <swayampatial1@gmail.com>
2025-04-08 00:52:35 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,13 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/navbar/Navbar";
-import Container from "@/components/global/Container";
-import Providers from "./providers";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/navbar/Navbar';
+import Container from '@/components/global/Container';
+import Providers from './providers';
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Next Store',
+  title: 'Next Storefront',
   description: 'A nifty store built with Next.js',
 };
 
@@ -26,15 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Providers>
-         <Navbar />
-         <Container className='py-20'>{children}</Container>
-         </Providers>
-      </body>
-    </html>
+      <html lang='en' suppressHydrationWarning>
+        <body className={inter.className}>
+          <Providers>
+            <Navbar />
+            <Container className='py-20'>{children}</Container>
+          </Providers>
+        </body>
+      </html>
   );
 }
