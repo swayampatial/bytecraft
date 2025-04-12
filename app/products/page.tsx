6030534cc33dcf9006ad05301e3b13e44962d1f0
swayam patial <swayampatial1@gmail.com>
2025-04-12 15:16:27 +0530

    first commit

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,13 +1,17 @@
 import ProductsContainer from '@/components/products/ProductsContainer';
 
-function ProductsPage({
-  searchParams,
-}: {
-  searchParams: { layout?: string; search?: string };
-}) {
-  const layout = searchParams.layout || 'grid';
-  const search = searchParams.search || '';
+type Props = {
+  searchParams?: {
+    layout?: string;
+    search?: string;
+  };
+};
+
+function ProductsPage({ searchParams }: Props) {
+  const layout = searchParams?.layout || 'grid';
+  const search = searchParams?.search || '';
 
   return <ProductsContainer layout={layout} search={search} />;
 }
+
 export default ProductsPage;
