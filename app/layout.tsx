882b612943df5e3c3534511a5630e9f954f6488e
swayam patial <swayampatial1@gmail.com>
2025-04-19 21:43:04 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Navbar from '@/components/navbar/Navbar';
 import Container from '@/components/global/Container';
 import Providers from './providers';
+import { ClerkProvider } from '@clerk/nextjs';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -16,14 +17,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-      <html lang='en' suppressHydrationWarning>
-        <body className={inter.className}>
-          <Providers>
-            <Navbar />
-            <Container className='py-20'>{children}</Container>
-          </Providers>
-        </body>
-      </html>
-  );
+    return (
+        <ClerkProvider>
+            <html lang='en' suppressHydrationWarning>
+            <body className={inter.className}>
+            <Providers>
+                <Navbar />
+                <Container className='py-20'>{children}</Container>
+            </Providers>
+            </body>
+            </html>
+        </ClerkProvider>
+    );
 }
