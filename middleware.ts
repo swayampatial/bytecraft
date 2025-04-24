923088690aa601c896bbd40a9184c6b8f0a55de4
swayam patial <swayampatial1@gmail.com>
2025-04-25 02:06:11 +0530

    first commit

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about"]);
 
 export default clerkMiddleware(async(auth, req) => {
-  if (!isPublicRoute(req)) auth.protect();
+  if (!isPublicRoute(req)) await auth.protect();
 });
 
 export const config = {
