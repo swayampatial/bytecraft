fb9e4253274a52b83c5bc5fc41ab0df801fa5a31
swayam patial <swayampatial1@gmail.com>
2025-04-25 23:41:26 +0530

    first commit

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,6 +1,27 @@
+'use client';
+
+import { SignOutButton } from '@clerk/nextjs';
+import { toast } from 'sonner'; // ✅ Correct import for toast from sonner
+import Link from 'next/link';
+
 function SignOutLink() {
+  const handleLogout = () => {
+    toast('Logging Out...');
+  };
+
   return (
-    <div>SignOutLink</div>
-  )
+    <SignOutButton>
+      <Link href='/' className='w-full text-left' onClick={handleLogout}>
+        Logout
+      </Link>
+    </SignOutButton>
+  );
 }
-export default SignOutLink
+
+export default SignOutLink;
+
+
+function useToast(): { toast: any; } {
+  throw new Error('Function not implemented.');
+}
+     
