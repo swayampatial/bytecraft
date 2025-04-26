de455bfe2279513ec76604c8e31e05a4578775af
swayam patial <swayampatial1@gmail.com>
2025-04-26 11:52:02 +0530

    first commit

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,13 @@
 import ProductsContainer from '@/components/products/ProductsContainer';
 
-export default async function ProductsPage({searchParams}: {
-    searchParams?: { layout?: string; search?: string };
+function ProductsPage({
+  searchParams,
+}: {
+  searchParams: { layout?: string; search?: string };
 }) {
-    const layout = searchParams?.layout || 'grid';
-    const search = searchParams?.search || '';
+  const layout = searchParams.layout || 'grid';
+  const search = searchParams.search || '';
 
-    return <ProductsContainer layout={layout} search={search}/>;
+  return <ProductsContainer layout={layout} search={search} />;
 }
+export default ProductsPage;
