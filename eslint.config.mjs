6e58f663e73138e6529b9251858ea4c2b969a234
swayam patial <swayampatial1@gmail.com>
2025-05-01 17:00:55 +0530

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,24 @@
 // eslint.config.mjs
-import { eslintPlugin as tseslint } from '@typescript-eslint/eslint-plugin';
-import parser from '@typescript-eslint/parser';
+import { eslintPlugin as tseslint } from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
 
 export default [
   {
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
       parser: parser,
       parserOptions: {
-        project: './tsconfig.json',
+        project: "./tsconfig.json",
       },
     },
-   
+
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+
     rules: {
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/explicit-function-return-type': 'off',
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
     },
   },
 ];
